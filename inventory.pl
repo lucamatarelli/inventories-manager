b286cda6a1f10c15549d57cfdf87e3392b515c45
Luca Matarelli <luca.matarelli@gmail.com>
2023-03-29 02:38:58 +0200

    Encodage + Amorce programme + .gitignore

--- inventory.pl
+++ inventory.pl
@@ -1,5 +1,20 @@
 use strict;
 use warnings;
+use utf8;
 
 use Storable;
 
+if ($^O eq "MSWin32") {
+    binmode STDOUT, ":encoding(CP-850)";
+} elsif ($^O eq "linux") {
+    binmode STDOUT, ":encoding(UTF-8)";
+}
+
+my @inventories_paths = glob "./inventories/*";
+
+if (!@inventories_paths) {
+    print "Aucun inventaire disponible\nVoulez-vous en créer un ? (o/n) ";
+    my $input = <STDIN>;
+} else {
+    print "Inventaires disponibles :\n";
+}
