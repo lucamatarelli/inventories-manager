d351b5384aed6276f5e40d73a3c29701981f6242
Luca Matarelli <luca.matarelli@gmail.com>
2023-03-29 04:08:35 +0200

    Inventory.pm : create_inventory() add_item() add_category()

--- Inventory.pm
+++ Inventory.pm
@@ -3,6 +3,33 @@ package Inventory;
 use strict;
 use warnings;
 
+sub create_inventory {
+    my @categories = @_;
+    my %new_inventory;
+    $new_inventory{$_} = [] for (@categories);
+    return %new_inventory;
+}
 
+sub add_item {
+    my ($inventory_ref, $category, $item) = @_;
+    push @{$inventory_ref->{$category}}, $item;
+    return 1;
+}
+
+sub edit_item {}
+
+sub remove_item {}
+
+sub add_category {
+    my ($inventory_ref, $category) = @_;
+    $inventory_ref->{$category} = [];
+    return 1;
+}
+
+sub rename_category {}
+
+sub move_category {}
+
+sub remove_category {}
 
 1;
--- inventories-manager.pl
+++ inventories-manager.pl
@@ -3,6 +3,12 @@ use warnings;
 use utf8;
 
 use Storable;
+use Data::Dump;
+use Cwd qw(abs_path);
+use File::Basename qw(dirname);
+use lib dirname(abs_path($0));
+
+use Inventory;
 
 if ($^O eq "MSWin32") {
     binmode STDOUT, ":encoding(CP-850)";
@@ -12,13 +18,21 @@ if ($^O eq "MSWin32") {
 
 my @inventories_paths = glob "./inventories/*";
 
-if (!@inventories_paths) {
-    print "Aucun inventaire disponible\nSouhaitez-vous en créer un ? (o/n) ";
-    my $input = <STDIN>;
-    while ($input !~ /^o\n|n\n$/i) {
-        print "Choix invalide. Souhaitez-vous créer votre premier inventaire ? (o/n) ";
-        $input = <STDIN>;
-    }
-} else {
-    print "Inventaires disponibles :\n";
-}
+# if (!@inventories_paths) {
+#     print "Aucun inventaire disponible\nSouhaitez-vous en créer un ? (o/n) ";
+#     my $input = <STDIN>;
+#     while ($input !~ /^o\n|n\n$/i) {
+#         print "Choix invalide. Souhaitez-vous créer votre premier inventaire ? (o/n) ";
+#         $input = <STDIN>;
+#     }
+# } else {
+#     print "Inventaires disponibles :\n";
+# }
+
+my %ninv = Inventory::create_inventory("chaussures", "pulls", "chemises");
+dd %ninv;
+Inventory::add_item(\%ninv, "chemises", "Boggi Milano bleu vichy Button down");
+Inventory::add_item(\%ninv, "pulls", "Chevignon cachemire marron");
+Inventory::add_item(\%ninv, "pulls", "Benjamin Jezequel rose gaufré");
+Inventory::add_category(\%ninv, "jeans");
+dd %ninv;
