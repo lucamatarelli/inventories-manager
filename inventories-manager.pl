bfd8832cad6624988106fb1d79b62dc156f0ed9c
Luca Matarelli <luca.matarelli@gmail.com>
2023-04-04 04:29:16 +0200

    inventories-manager.pl : boucle du programme principal + do_action() avec gestion du déplacement vers sous-catégorie

--- inventories-manager.pl
+++ inventories-manager.pl
@@ -43,7 +43,37 @@ sub display_inventory_options {
 }
 
 sub do_action {
+    my ($option_number, $curr_category_ref) = @_;
+    if ($option_number eq "1") {
+        print "Déplacement vers quelle catégorie ? ";
+        my @subcategories = grep {$_ ne "items"} keys %$curr_category_ref;
+        my $subcategories_disjunction = join "|", @subcategories;
+        my $new_category_ref = input_check(qr/^($subcategories_disjunction)$/, "Veuillez entrer un nom de catégorie valide : ");
+        $curr_category_ref = $curr_category_ref->{$new_category_ref};
+        return $curr_category_ref;
+     } elsif ($option_number eq "2") {
 
+    } elsif ($option_number eq "3") {
+        
+    } elsif ($option_number eq "4") {
+        
+    } elsif ($option_number eq "5") {
+        
+    } elsif ($option_number eq "6") {
+        
+    } elsif ($option_number eq "7") {
+        
+    } elsif ($option_number eq "8") {
+        
+    } elsif ($option_number eq "9") {
+        
+    } elsif ($option_number eq "10") {
+        
+    } elsif ($option_number eq "11") {
+        return "EXIT";
+    } elsif ($option_number eq "12") {
+        return "EXIT";        
+    }
 }
 
 my @inventories_paths = glob "./inventories/*";
@@ -52,6 +82,7 @@ my $input = "";
 if (scalar @inventories_paths == 0) {
     print "Aucun inventaire disponible.\nSouhaitez-vous en créer un ? (o/n) ";    
     $input = input_check(qr/^[on]$/i, "Choix invalide. Souhaitez-vous créer votre premier inventaire ? (o/n) ");
+
     if ($input =~ /^o$/i) {
         print "\nNommez votre nouvel inventaire : ";
         my $new_inventory_name = input_check(qr/^[\p{L}\d -]+$/, "Nom d'inventaire non valide. Réessayez avec un format valide : ");
@@ -72,7 +103,17 @@ if (scalar @inventories_paths == 0) {
         my %new_inventory = new_inventory(@macrocategories);
         print "\nNouvel inventaire créé !\n\n";
         sleep 2;
-        my $choice = display_inventory_options(\%new_inventory, $new_inventory_name);
+
+        my $curr_category_ref = \%new_inventory;
+        while (1) {
+            my $choice = display_inventory_options($curr_category_ref, $new_inventory_name);
+            my $action_result = do_action($choice, $curr_category_ref);
+            if (ref $action_result eq "HASH") {
+                $curr_category_ref = $action_result;
+            } else {
+                last if $action_result eq "EXIT";
+            }
+        }
     }
 } else {
     print "Inventaires disponibles :\n";
